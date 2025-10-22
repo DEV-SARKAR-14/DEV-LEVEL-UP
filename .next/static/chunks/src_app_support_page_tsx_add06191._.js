b16a60371d68612117b2617c4654b7b0e3494645
DEV-SARKAR-14 <debojitmondal17@gmail.com>
2025-10-22 13:47:14 +0530

    Upload src_app_support_page_tsx_add06191._.js

---
+++ .next/static/chunks/src_app_support_page_tsx_add06191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_383f4cb8._.js",
+  "static/chunks/node_modules_e15166fa._.js"
+],
+    source: "dynamic"
+});
