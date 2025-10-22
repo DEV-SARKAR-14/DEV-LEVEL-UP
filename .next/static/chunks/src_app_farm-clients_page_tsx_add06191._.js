4bde232e6916eb482c6e9e6a4a1099c896175c15
DEV-SARKAR-14 <debojitmondal17@gmail.com>
2025-10-22 13:46:48 +0530

    Upload src_app_farm-clients_page_tsx_add06191._.js

---
+++ .next/static/chunks/src_app_farm-clients_page_tsx_add06191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_47666d80._.js",
+  "static/chunks/node_modules_57440a50._.js"
+],
+    source: "dynamic"
+});
