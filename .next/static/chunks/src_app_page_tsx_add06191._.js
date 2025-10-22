dd0ed56c7a0e1fe73949f546b987b261591f5e1a
DEV-SARKAR-14 <debojitmondal17@gmail.com>
2025-10-22 13:47:12 +0530

    Upload src_app_page_tsx_add06191._.js

---
+++ .next/static/chunks/src_app_page_tsx_add06191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_362154ea._.js",
+  "static/chunks/node_modules_d92f3582._.js"
+],
+    source: "dynamic"
+});
